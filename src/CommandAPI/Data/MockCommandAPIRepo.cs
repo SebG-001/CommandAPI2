46e74a3d8049281f1b0d0f7a763308e310d89c9c
Sebastian Golda <sgolda001@gmail.com>
2021-05-12 02:14:54 -0700

    Added Database Context

--- src/CommandAPI/Data/MockCommandAPIRepo.cs
+++ src/CommandAPI/Data/MockCommandAPIRepo.cs
@@ -26,11 +26,11 @@ namespace CommandAPI.Data
                     Id=0, HowTo="How to generate a migration",
                     CommandLine="dotnet ef migrations add <Name of Migration>",
                     Platform=".Net Core EF" },
-                new Command{
+                new Command {
                     Id=1, HowTo="Run Migrations",
                     CommandLine="dotnet ef database update",
                     Platform=".Net Core EF"},
-                new Command{
+                new Command {
                     Id=2, HowTo="List active migrations",
                     CommandLine="dotnet ef migrations list",
                     Platform=".Net Core EF"}
